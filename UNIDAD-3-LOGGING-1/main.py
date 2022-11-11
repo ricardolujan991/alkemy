5f12a5dfc2596b8ab9183c0be99c87e3e687883c
ricardo lujan <101593476+ricardolujan991@users.noreply.github.com>
2022-11-11 15:29:39 -0300

    UNIDAD-3-LOGGING-1

--- UNIDAD-3-LOGGING-1/main.py
+++ UNIDAD-3-LOGGING-1/main.py
@@ -4,7 +4,6 @@ logging.Formatter(fmt=' %(name)s - %(levelname)s - %(message)s')
 fruits = ['Frutilla','Melón','PERA',99.6,'NaranJA', 'mORa', 'NisPERo',99]
 for frut in fruits:
     try:
-    
         logging.info(f'conversion exitosa : {frut} ---> {frut.lower()}')
     except:
         logging.error(f'conversion fallida : {frut}')
